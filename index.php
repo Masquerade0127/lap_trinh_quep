e3339ccb71e896045089715db7cffb4a84ea403c
Le Hai Trieu <lhtrieu.ktpm0115@student.ctuet.edu.vn>
2018-10-29 12:53:30 +0700

    them trang home

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
-include("html/component/header.html")
-
-include("html/component/footer.html")
+<?php
+	include("html/component/header.html");
+	include("html/component/menu.html")
+?>
+<div id="content">
+	<img src="image/bgr_content.png" alt="content" style="width: 100%;">
+</div>
+<?php
+	include("html/component/footer.html");
+?>
