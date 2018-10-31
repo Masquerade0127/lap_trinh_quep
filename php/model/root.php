25cad64230259673e3b7bc5cc7bf87c6f81b0e6c
Le Hai Trieu <lhtrieu.ktpm0115@student.ctuet.edu.vn>
2018-10-31 11:10:31 +0700

    edit Root.php, them check loggin

--- php/model/root.php
+++ php/model/root.php
@@ -2,26 +2,41 @@
 require_once('Connect.php');
 class Root{
 	private $id;
-	/*
-	 * get id
-	 * input: none
-	 * output: id
-	 */
+
+	/*get ID*/
 	function getId(){
 		return $this->id;
 	}
 
 	/*
-	 * lấy tên cột theo id 
-	 * input: id 
-	 * output: tên khoa
+	 * lay gia tri cot cua bang theo od
+	 * input: id, table, column
+	 * output: mang cac gia tri cua cot
 	 */
 	function getColumnValue($id, $table, $column){
 		$query = "select $column from $table where ID=$id";
-		if($result = mysqli_query(Connect::getConnect(),$query)){
-			$row = mysqli_fetch_assoc($result);
-			return $row["$column"];
-		}	
+		$result = Connect::getConnect()->query($query);
+		$index = 0;
+		while ($row = mysqli_fetch_assoc($result)) {
+			$array[$index] = $row;
+			$index++;
+		}
+		return $array;
+	}
+
+	/*
+	 * kiem tra dang nhap
+	 * input: none
+	 * output: none
+	 */
+	function checkLoggin(){
+		if (!isset($_SESSION["id"]) && !isset($_SESSION["password"])) {
+			header("http://localhost/lap_trinh_quep/loggin.html");
+		}
 	}
 }
+/*$khoa = new Root();
+foreach ($khoa->getColumnValue(2,"khoa","*") as $key) {
+ 	echo $key["ten_khoa"];
+}*/
 ?>
