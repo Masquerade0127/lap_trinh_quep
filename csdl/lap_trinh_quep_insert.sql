5aa3d4131555081339e88cbc0ee737b2f9c2a5fe
kimkhue12345 <kimkhue123456789@gmail.com>
2018-11-08 23:31:19 +0700

    edit csdl

--- csdl/lap_trinh_quep_insert.sql
+++ csdl/lap_trinh_quep_insert.sql
@@ -1,6 +1,6 @@
 ﻿
 
-use lap_trinh_quep;
+use k1;
 insert into khoa(ten_khoa)
 values (N'công nghệ thông tin');
 insert into khoa(ten_khoa)
@@ -24,10 +24,95 @@ values (3, N'Đặng Thị Mông Vui', '1997-01-20', N'Trà Vinh', '0906159062',
 
 insert into mon(ma_nganh, ten_mon)
 values (3, N'kiến trúc máy tính');
-insert into mon(ma_nganh, ten_mon)
-values (3, N'kỹ thuật lập trình');
-insert into mon(ma_nganh, ten_mon)
-values (3, N'toán rời rạc');
 
-insert into dap_an(noi_dung, trang_thai)
-values (N'là hệ quản trị csdl', 'sai');
+insert into cau_hoi(noi_dung, muc_do)
+values (N'Hệ quản trị CSDL DBMS (DataBase Management System) làd:', N'Dễ');
+insert into cau_hoi(noi_dung, muc_do)
+values (N'Hệ quản trị CSDL DBMS (DataBase Management System) làd1:', N'Dễ');
+insert into cau_hoi(noi_dung, muc_do)
+values (N'Hệ quản trị CSDL DBMS (DataBase Management System) làd2:', N'Dễ');
+insert into cau_hoi(noi_dung, muc_do)
+values (N'Hệ quản trị CSDL DBMS (DataBase Management System) làd3:', N'Dễ');
+insert into cau_hoi(noi_dung, muc_do)
+values (N'Hệ quản trị CSDL DBMS (DataBase Management System) làd4:', N'Dễ');
+insert into cau_hoi(noi_dung, muc_do)
+values (N'Hệ quản trị CSDL DBMS (DataBase Management System) làd5:', N'Dễ');
+insert into cau_hoi(noi_dung, muc_do)
+values (N'Hệ quản trị CSDL DBMS (DataBase Management System) làd6:', N'Dễ');
+insert into cau_hoi(noi_dung, muc_do)
+values (N'Hệ quản trị CSDL DBMS (DataBase Management System) làd7:', N'Dễ');
+insert into cau_hoi(noi_dung, muc_do)
+values (N'Hệ quản trị CSDL DBMS (DataBase Management System) làd8:', N'Dễ');
+insert into cau_hoi(noi_dung, muc_do)
+values (N'Hệ quản trị CSDL DBMS (DataBase Management System) làd9:', N'Dễ');
+insert into cau_hoi(noi_dung, muc_do)
+values (N'Hệ quản trị CSDL DBMS (DataBase Management System) làd10:', N'Dễ');
+insert into cau_hoi(noi_dung, muc_do)
+values (N'Hệ quản trị CSDL DBMS (DataBase Management System) làd11:', N'Dễ');
+insert into cau_hoi(noi_dung, muc_do)
+values (N'Hệ quản trị CSDL DBMS (DataBase Management System) làd12:', N'Dễ');
+insert into cau_hoi(noi_dung, muc_do)
+values (N'Hệ quản trị CSDL DBMS (DataBase Management System) làd13:', N'Dễ');
+insert into cau_hoi(noi_dung, muc_do)
+values (N'Hệ quản trị CSDL DBMS (DataBase Management System) làd14:', N'Dễ');
+insert into cau_hoi(noi_dung, muc_do)
+values (N'Hệ quản trị CSDL DBMS (DataBase Management System) làd15:', N'Dễ');
+insert into cau_hoi(noi_dung, muc_do)
+values (N'Hệ quản trị CSDL DBMS (DataBase Management System) làd16:', N'Dễ');
+insert into cau_hoi(noi_dung, muc_do)
+values (N'Hệ quản trị CSDL DBMS (DataBase Management System) làd17:', N'Dễ');
+insert into cau_hoi(noi_dung, muc_do)
+values (N'Hệ quản trị CSDL DBMS (DataBase Management System) làd18:', N'Dễ');
+insert into cau_hoi(noi_dung, muc_do)
+values (N'Hệ quản trị CSDL DBMS (DataBase Management System) làd19:', N'Dễ');
+insert into cau_hoi(noi_dung, muc_do)
+values (N'Hệ quản trị CSDL DBMS (DataBase Management System) làd20:', N'Dễ');
+insert into cau_hoi(noi_dung, muc_do)
+values (N'Hệ quản trị CSDL DBMS (DataBase Management System) làk:', N'khó');
+insert into cau_hoi(noi_dung, muc_do)
+values (N'Hệ quản trị CSDL DBMS (DataBase Management System) làk1:', N'khó');
+insert into cau_hoi(noi_dung, muc_do)
+values (N'Hệ quản trị CSDL DBMS (DataBase Management System) làk2:', N'khó');
+insert into cau_hoi(noi_dung, muc_do)
+values (N'Hệ quản trị CSDL DBMS (DataBase Management System) làk3:', N'khó');
+insert into cau_hoi(noi_dung, muc_do)
+values (N'Hệ quản trị CSDL DBMS (DataBase Management System) làk4:', N'khó');
+insert into cau_hoi(noi_dung, muc_do)
+values (N'Hệ quản trị CSDL DBMS (DataBase Management System) làk5:', N'khó');
+insert into cau_hoi(noi_dung, muc_do)
+values (N'Hệ quản trị CSDL DBMS (DataBase Management System) làt:', N'TB');
+insert into cau_hoi(noi_dung, muc_do)
+values (N'Hệ quản trị CSDL DBMS (DataBase Management System) làt1:', N'TB');
+insert into cau_hoi(noi_dung, muc_do)
+values (N'Hệ quản trị CSDL DBMS (DataBase Management System) làt2:', N'TB');
+insert into cau_hoi(noi_dung, muc_do)
+values (N'Hệ quản trị CSDL DBMS (DataBase Management System) làt3:', N'TB');
+insert into cau_hoi(noi_dung, muc_do)
+values (N'Hệ quản trị CSDL DBMS (DataBase Management System) làt4:', N'TB');
+insert into cau_hoi(noi_dung, muc_do)
+values (N'Hệ quản trị CSDL DBMS (DataBase Management System) làt5:', N'TB');
+insert into cau_hoi(noi_dung, muc_do)
+values (N'Hệ quản trị CSDL DBMS (DataBase Management System) làt6:', N'TB');
+insert into cau_hoi(noi_dung, muc_do)
+values (N'Hệ quản trị CSDL DBMS (DataBase Management System) làt7:', N'TB');
+insert into cau_hoi(noi_dung, muc_do)
+values (N'Hệ quản trị CSDL DBMS (DataBase Management System) làt8:', N'TB');
+insert into cau_hoi(noi_dung, muc_do)
+values (N'Hệ quản trị CSDL DBMS (DataBase Management System) làt9:', N'TB');
+insert into cau_hoi(noi_dung, muc_do)
+values (N'Hệ quản trị CSDL DBMS (DataBase Management System) làt10:', N'TB');
+insert into cau_hoi(noi_dung, muc_do)
+values (N'Hệ quản trị CSDL DBMS (DataBase Management System) làt11:', N'TB');
+insert into cau_hoi(noi_dung, muc_do)
+values (N'Hệ quản trị CSDL DBMS (DataBase Management System) làt12:', N'TB');
+insert into cau_hoi(noi_dung, muc_do)
+values (N'Hệ quản trị CSDL DBMS (DataBase Management System) làt13:', N'TB');
+insert into cau_hoi(noi_dung, muc_do)
+values (N'Hệ quản trị CSDL DBMS (DataBase Management System) làt14:', N'TB');
+insert into cau_hoi(noi_dung, muc_do)
+values (N'Hệ quản trị CSDL DBMS (DataBase Management System) làt15:', N'TB');
+insert into cau_hoi(noi_dung, muc_do)
+values (N'Hệ quản trị CSDL DBMS (DataBase Management System) làt16:', N'TB');
+insert into cau_hoi(noi_dung, muc_do)
+values (N'Hệ quản trị CSDL DBMS (DataBase Management System) làt17:', N'TB');
+
