6dab2bfd4bef38eccd3a3ceaf62a6721019c9de2
vuidangg <43978542+vuidangg@users.noreply.github.com>
2018-11-09 21:29:07 +0700

    edit page tao cau hoi

--- php/views/taocauhoi.php
+++ php/views/taocauhoi.php
@@ -2,13 +2,13 @@
 	include("../../html/component/header.html");
 	include("../../html/component/menu.html")
 ?>
-<link rel="stylesheet" type="text/css" href="../../css/taocauhoi.css">
-<div id="content" class="col-12">
-	<div id="user">
-			<div class="btn-group" role="group" aria-label="Button group with nested dropdown" style="margin-top: 1%">
-  				<div class="file-attachment-group" style="background-color: white;height: 50px">
-					<p><img src="../../image/icon_admin.png" /></p>
-    				<select name="cars" class="form-control" style="color:blue;width: 100%"> 
+<link rel="stylesheet" type="text/css" href="../../../css/taocauhoi.css">
+<div id="content"style="height: 1950px">
+	<div id="user" class="col-12">
+			<div class="btn-group" role="group" aria-label="Button group with nested dropdown" style="margin-top: 1%;text-align: right;border-radius: 10px">
+  				<div class="file-attachment-group" style="background-color: white;height: 60px;width: 100%">
+					<p><img style="width: 100%;height: 100%" src="../../../image/icon_admin.png" /></p>
+    				<select name="cars" class="form-control" style="color:blue;width:100%"> 
       					<option>Lê Hải Triều</option>
       					<option>Đăng xuất</option>
       					<option>Tài khoản khác</option>
@@ -17,19 +17,11 @@
   			</div>
 	</div>
 	<div id="noidung">
-		<div class="col-3" id="menu_left">
-			<ul style="margin-top: 5%" class="col-12">
-				<a href="#" style="color: blue"><img src="../../image/icon_class.png"> Kỹ Thuật Phần Mềm</a>
-				<hr width="80%" />
-				<a href="#" style="color: blue"><img src="../../image/icon_class.png"> Kỹ Thuật Phần Mềm</a>
-				<hr width="80%" />
-				<a href="#" style="color: blue"><img src="../../image/icon_class.png"> Kỹ Thuật Phần Mềm</a>
-			<ul>
-		</div>
-		<div class="col-8" id="menu_right">
-			<div class="form-create-question-wrapper">
+		<div class="col-3" id="menu_left"></div>
+		<div class="col-8" id="menu_right"">
+			<div class="form-create-question-wrapper" style="margin-top: 5%">
 					<form action="/action_page.php" id="form-create-question" method="post" enctype="multipart/form-data">
-						<h1 style="text-align: center;">TẠO CÂU HỎI<img src="../../image/icon_author.png" alt="icon author"/></h1>
+						<h1 style="text-align: center;">TẠO CÂU HỎI<img src="../../../image/icon_author.png" alt="icon author"/></h1>
 						<b><h3><span style="color: black">CÂU 1</span></h3></b>
 							<div class="form-group">
 								<div style="color: black">* Nội dung câu hỏi:</div>
@@ -41,7 +33,7 @@
 					    					<option value="volvo">Trung bình</option>
 					   						<option value="fiat">Khó</option>
 					   					 </select>
-					   			 		<input style="border-radius: 5%;background-color: #0033CC;width: 100%" type="file" class="alert-link" name="image_cau_hoi"></input>
+					   			 		<input style="border-radius: 10px;background-color: #0033CC;width: 100%" type="file" class="alert-link" name="image_cau_hoi"></input>
 									</div>
 								</div>
 								<textarea class="form-control" rows="3" id="comment" name="text"></textarea>
@@ -58,7 +50,7 @@
 					    					<option selected>Sai</option>
 					    					<option value="volvo">Đúng</option>
 					   					 </select>
-					   			 		<input style="border-radius: 5%;background-color: #0033CC;width: 100%" type="file" class="alert-link" name="image_cau_hoi"></input>
+					   			 		<input style="border-radius: 10px;background-color: #0033CC;width: 100%" type="file" class="alert-link" name="image_cau_hoi"></input>
 									</div>
 								</div>
 								<textarea class="form-control" rows="3" id="comment" name="text"></textarea>
@@ -73,7 +65,7 @@
 					    					<option selected>Sai</option>
 					    					<option value="volvo">Đúng</option>
 					   					 </select>
-					   			 		<input style="border-radius: 5%;background-color: #0033CC;width: 100%" type="file" class="alert-link" name="image_cau_hoi"></input>
+					   			 		<input style="border-radius: 10px;background-color: #0033CC;width: 100%" type="file" class="alert-link" name="image_cau_hoi value="thêm tệp></input>
 									</div>
 								</div>
 								<textarea class="form-control" rows="3" id="comment" name="text"></textarea>
@@ -88,7 +80,7 @@
 					    					<option selected>Sai</option>
 					    					<option value="volvo">Đúng</option>
 					   					 </select>
-					   			 		<input style="border-radius: 5%;background-color: #0033CC;width: 100%" type="file" class="alert-link" name="image_cau_hoi"></input>
+					   			 		<input style="border-radius: 10px;background-color: #0033CC;width: 100%" type="file" class="alert-link" name="image_cau_hoi"></input>
 									</div>
 								</div>
 								<textarea class="form-control" rows="3" id="comment" name="text"></textarea>
@@ -103,7 +95,7 @@
 					    					<option selected>Sai</option>
 					    					<option value="volvo">Đúng</option>
 					   					 </select>
-					   			 		<input style="border-radius: 5%;background-color: #0033CC;width: 100%" type="file" class="alert-link" name="image_cau_hoi"></input>
+					   			 		<input style="border-radius: 10px;background-color: #0033CC;width: 100%" type="file" class="alert-link" name="image_cau_hoi"></input>
 									</div>
 								</div>
 								<textarea class="form-control" rows="3" id="comment" name="text"></textarea>
@@ -118,7 +110,7 @@
 					    					<option selected>Sai</option>
 					    					<option value="volvo">Đúng</option>
 					   					 </select>
-					   			 		<input style="border-radius: 5%;background-color: #0033CC;width: 100%" type="file" class="alert-link" name="image_cau_hoi"></input>
+					   			 		<input style="border-radius: 10px;background-color: #0033CC;width: 100%" type="file" class="alert-link" name="image_cau_hoi"></input>
 									</div>
 								</div>
 								<textarea class="form-control" rows="3" id="comment" name="text"></textarea>
@@ -133,7 +125,7 @@
 					    					<option selected>Sai</option>
 					    					<option value="volvo">Đúng</option>
 					   					 </select>
-					   			 		<input style="border-radius: 5%;background-color: #0033CC;width: 100%" type="file" class="alert-link" name="image_cau_hoi"></input>
+					   			 		<input style="border-radius: 10px;background-color: #0033CC;width: 100%" type="file" class="alert-link" name="image_cau_hoi"></input>
 									</div>
 								</div>
 								<textarea class="form-control" rows="3" id="comment" name="text"></textarea>
@@ -141,9 +133,9 @@
 						</div>
 						<div class="button-form-submit">
 							<button type="submit" class="btn btn-secondary" style="margin-right: 20px;">
-							<img src="../../image/icon_ok.png" alt="icon ok" style=""/>LƯU</button>
+							<img src="../../../image/icon_ok.png" alt="icon ok" style="width: 60%;height: 30px"/>LƯU</button>
 							<button type="submit" class="btn btn-secondary">
-							<img src="../../image/icon_exit.png" alt="icon link" style=""/>HỦY</button>
+							<img src="../../../image/icon_exit.png" alt="icon link" style="width: 60%;height: 30px"/>HỦY</button>
 						</div>
 						<div style="height: 25px"></div>
 				  </form>
@@ -151,11 +143,10 @@
 		</div>
 	</div>		
 </div>
-
 <?php
-	include("../../html/component/footer.html");
+	include("../../html/component/footer.html")
 ?>
- 
+
 
 	           
 	            
