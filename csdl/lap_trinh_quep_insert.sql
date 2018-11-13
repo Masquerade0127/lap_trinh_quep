89a3057b5cad2b11a0a19fa8f25747245eb6b7fe
kenpiece <32983123+kenpiece@users.noreply.github.com>
2018-11-13 23:51:11 +0700

    Sửa csdl các bảng lớp, môn...(trùng mã)

--- csdl/lap_trinh_quep_insert.sql
+++ csdl/lap_trinh_quep_insert.sql
@@ -19,17 +19,17 @@ values ('2015-09-01', '2015-09-01', 'k3');
 insert into lop(ma_nganh, ma_nien_khoa, ten_lop)
 values (3, 1, N'Kỹ thuật phần mềm');
 insert into lop(ma_nganh, ma_nien_khoa, ten_lop)
-values (3, 1, N'Khoa học máy tính');
+values (1, 1, N'Khoa học máy tính');
 insert into lop(ma_nganh, ma_nien_khoa, ten_lop)
-values (3, 1, N'Hệ thống thông tin');
+values (2, 1, N'Hệ thống thông tin');
 
 insert into giang_vien(ma_nganh, ten_gv, ngay_sinh, dia_chi, sdt, email)
 values (3, N'Đặng Thị Mông Vui', '1997-01-20', N'Trà Vinh', '0906159062', 'dtmvui.ktpm0115@student.ctuet.edu.vn');
 
 insert into mon(ma_nganh, ten_mon)
-values (3, N'kiến trúc máy tính');
+values (1, N'kiến trúc máy tính');
 insert into mon(ma_nganh, ten_mon)
-values (3, N'kỹ thuật lập trình');
+values (2, N'kỹ thuật lập trình');
 insert into mon(ma_nganh, ten_mon)
 values (3, N'toán rời rạc');
 
