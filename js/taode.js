15a0b177a8aab2b3cfbaec03c22eb03aa2b0ad4d
vuidangg <43978542+vuidangg@users.noreply.github.com>
2018-11-13 14:36:28 +0700

    thêm ajax js

--- js/taode.js
+++ js/taode.js
@@ -41,3 +41,15 @@ document.addEventListener("DOMContentLoaded", function(){
 		}
 	}
 },false)
+//ajax tao de
+function loadDoc()	{
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("demo").innerHTML =
+      this.responseText;
+    }
+  };
+  xhttp.open("GET", "gethint.php", true);
+  xhttp.send();
+}
