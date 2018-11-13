194639af1a1a81929c93c2528c1390af5a9e971e
kenpiece <32983123+kenpiece@users.noreply.github.com>
2018-11-13 23:25:29 +0700

    Add hàm lấy URL

--- php/process/danh_sach_mon.php
+++ php/process/danh_sach_mon.php
@@ -1,5 +1,8 @@
 <?php
 require("../model/Mon.php");
+require_once("C:/xampp/htdocs/lap_trinh_quep/php/model/root.php");
+$url = new Root();
+$url1 = $url->getCurrentPageURL();
 $mon = new Mon();
-$danh_sach = $mon->getColumn("mon");
+$danh_sach = $mon->getColumn($url1);
 ?>
